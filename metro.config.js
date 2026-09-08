90daf1faeca5e5d14496bb4e7d9e4513ab6bca78
Your Name <you@example.com>
2026-09-08 19:03:50 +0200

    Fix Metro config - remove invalid watcher options, app running on port 8082

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,8 @@ const config = getDefaultConfig(__dirname);
 config.watcher = {
   ...config.watcher,
   unstable_disableWatchman: true,
+  // Reduce file watcher pressure
+  maxWatchers: 50,
 };
 
 module.exports = config;
